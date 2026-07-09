7f1c1359afd2e3469e9c3378b493553b1639cf06
kroko <krokon199@gmail.com>
2026-07-09 10:46:49 +0600

    build: declare plugin id via java-gradle-plugin

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `kotlin-dsl`
+    `java-gradle-plugin`
 }
 
 group = "com.rogad"
@@ -7,4 +8,16 @@ version = "0.1.0"
 
 repositories {
     mavenCentral()
+}
+
+gradlePlugin {
+    plugins {
+        create("bracketCounter") {
+            id = "com.rogad.bracket-counter"
+            implementationClass = "com.rogad.bracketcounter.BracketCounterPlugin"
+            displayName = "Bracket Counter Plugin"
+            description = "Adds a text file with the number of opening brackets " +
+                    "per source file into the final JAR of a Java module."
+        }
+    }
 }
